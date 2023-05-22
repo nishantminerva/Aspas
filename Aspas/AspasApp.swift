2e97c4454f95866829ce3367c9098704752a9d31
Nishant Minerva <nishantcafe@gmail.com>
2023-05-22 16:23:20 +0530

    Initial Commit

---
+++ Aspas/AspasApp.swift
@@ -0,0 +1,20 @@
+//
+//  AspasApp.swift
+//  Aspas
+//
+//  Created by Nishant Minerva on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AspasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
